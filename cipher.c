a18131dd3325386ddb8d965f242fd904547d0fd3
Andrew Campbell <82439783+campbellas@users.noreply.github.com>
2024-05-08 11:53:57 -0400

    Update cipher.c
    If the second command line argument is used with quotation marks, you can have punctuation and white spaces now.

--- cipher.c
+++ cipher.c
@@ -29,6 +29,10 @@ int main(int argc, char *argv[]) {
 				printf("%c", encode_it);
 				
 			}
+			//Handles all special characters and white spaces
+			else if((encode_it > 31 && encode_it < 65) || (encode_it > 90 && encode_it < 97) || (encode_it > 122 && encode_it < 127)) {
+				printf("%c", encode_it);
+			}
 			else {
 				printf("ERROR: %c is not a valid letter", encode_it);
 			}
@@ -60,6 +64,10 @@ int main(int argc, char *argv[]) {
 				}
 				printf("%c", decode_it);
 			
+			}
+			//Handles all special characters and whitespaces
+			else if((decode_it > 31 && decode_it < 65) || (decode_it > 90 && decode_it < 97) || (decode_it > 122 && decode_it < 127)) {
+				printf("%c", decode_it);
 			}
 			else {
 				printf("ERROR: %c is not a valid letter", decode_it);
