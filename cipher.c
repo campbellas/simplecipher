080e7f634550fcc52fa6a967f53538657468f0a5
Andrew Campbell <82439783+campbellas@users.noreply.github.com>
2024-05-08 09:39:37 -0400

    Update cipher.c
    No user input here beyond command line arguments. The information encoded can now be put back into the terminal to decode it. 
    Encode: 
    cipher [toBeEncoded] e
    cipher [toBeEncoded] E
    Decode:
    cipher [toBeDecoded] d
    cipher [toBeDecoded] D

--- cipher.c
+++ cipher.c
@@ -1,33 +1,74 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main(int argc, char *argv[]) {
 	
-	for(int i=0; argv[1][i] != '\0'; i++) {
-		char letter = argv[1][i];
+	if((strcmp(argv[2], "e") == 0) || (strcmp(argv[2], "E") == 0)) {
+		//Encodes input
+		for(int i=0; argv[1][i] != '\0'; i++) {
+			char encode_it = argv[1][i];
 		
-		//UPPERCASE HANDLING: A = 65 Z = 90
-		if(letter > 64 && letter < 91){
-			//incremental step up to check for exceeding alphanumeric value.
-			for(int count = 0; count < 4; count++){
-				letter = letter + 1;
-				if(letter > 90) letter = 65;
-			}
-			printf("%c", letter);
+			//UPPERCASE HANDLING: A = 65 Z = 90
+			if(encode_it > 64 && encode_it < 91){
+				//incremental step up to check for exceeding alphanumeric value.
+				for(int count = 0; count < 4; count++){
+					encode_it = encode_it + 1;
+					if(encode_it > 90) encode_it = 65;
+				}
+				printf("%c", encode_it);
 			
-		}
-		//LOWERCASE HANDLING: a = 97 z = 122.
-		else if(letter > 96 && letter < 123) {
-			//incremental step up to check for exceeding alphanumeric value.
-			for(int count = 0; count < 4; count++){
-				letter = letter + 1;
-				if(letter > 122) letter = 97;
 			}
-			printf("%c", letter);
+			//LOWERCASE HANDLING: a = 97 z = 122.
+			else if(encode_it > 96 && encode_it < 123) {
+				//incremental step up to check for exceeding alphanumeric value.
+				for(int count = 0; count < 4; count++){
+					encode_it = encode_it + 1;
+					if(encode_it > 122) encode_it = 97;
+				}
+				printf("%c", encode_it);
+				
+			}
+			else {
+				printf("ERROR: %c is not a valid letter", encode_it);
+			}
+		}
+	}
+		
+	else if((strcmp(argv[2], "d") == 0) || (strcmp(argv[2], "E") == 0)) {
+		
+		//Decodes input
+		for(int i=0; argv[1][i] != '\0'; i++) {
+			char decode_it = argv[1][i];
 			
+			//UPPERCASE HANDLING: A = 65 Z = 90
+			if(decode_it > 64 && decode_it < 91){
+				//incremental step up to check for exceeding alphanumeric value.
+				for(int count = 0; count < 4; count++){
+					decode_it = decode_it - 1;
+					if(decode_it < 65) decode_it = 90;
+				}
+				printf("%c", decode_it);
+				
+			}
+			//LOWERCASE HANDLING: a = 97 z = 122.
+			else if(decode_it > 96 && decode_it < 123) {
+				//incremental step up to check for exceeding alphanumeric value.
+				for(int count = 0; count < 4; count++){
+					decode_it = decode_it - 1;
+					if(decode_it < 97) decode_it = 122;
+				}
+				printf("%c", decode_it);
+			
+			}
+			else {
+				printf("ERROR: %c is not a valid letter", decode_it);
+			}
 		}
-		else {
-			printf("ERROR: %c is not a valid letter", letter);
-		}
+	}
+		
+	else {
+		printf("ERROR: Improper command line argument. Enter 'cipher [words to cipher] [e or d]");
 	}
 	
 	return 0;
